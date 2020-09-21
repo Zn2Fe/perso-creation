4e74ca2dfd718b1e73b422048d01a8d9a196d875
Zn2Fe <nicolas.devaux-dias@sfr.fr>
2020-09-21 17:14:49 +0200

    first version

---
+++ client-app/main.js
@@ -0,0 +1,13 @@
+// #region class definition
+class perso{
+    constructor(_id,_data){
+        this.id = _id;
+        this.data = _data;
+    }
+}
+// #endregion
+
+// #region global variables
+let persolist = [];
+let itemlist = [];
+// //#endregion
